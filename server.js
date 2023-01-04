812f29680b1ca2cf0228d3d232913de270f8f864
Pichborith Kong <pichborith@se2nd-air.attlocal.net>
2023-01-04 14:30:52 -0800

    add profile and image route

--- server.js
+++ server.js
@@ -53,6 +53,27 @@ app.post('/register', (req, res) => {
   res.json(database.users[database.users.length - 1]);
 });
 
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      return res.json(user);
+    }
+  });
+  res.status(400).json('no such user');
+});
+
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  res.status(400).json('no such user');
+});
+
 app.listen(3000, () => {
   console.log('app is running on port 3000');
 });
