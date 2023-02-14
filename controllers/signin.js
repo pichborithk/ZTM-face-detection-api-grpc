dd787115245c53590edbd1b10799def069dcbb07
Pichborith Kong <pichborith@gmail.com>
2023-02-14 12:57:09 -0800

    change signin function to verify sign and create authentication token but not send user data

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,24 +1,59 @@
-const handleSignin = (db, bcrypt) => (req, res) => {
+const jwt = require('jsonwebtoken');
+
+const handleSignin = (db, bcrypt, req, res) => {
   if (!req.body.email || !req.body.password) {
-    return res.status(400).json('incorrect form submission');
+    return Promise.reject('incorrect form submission');
   }
-  db.select('email', 'hash')
-    .from('login')
-    .where({ email: req.body.email })
-    .then((data) => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-      if (isValid) {
-        return db
-          .select('*')
-          .from('users')
-          .where({ email: req.body.email })
-          .then((data) => res.json(data[0]))
-          .catch((err) => res.status(400).json('err 1'));
-      } else {
-        res.status(400).json('err 2');
-      }
-    })
-    .catch((err) => res.status(400).json('err 3'));
+  return (
+    db
+      .select('email', 'hash')
+      .from('login')
+      .where({ email: req.body.email })
+      // .where('email', '=', req.body.email)
+      .then((data) => {
+        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+        if (isValid) {
+          return db
+            .select('*')
+            .from('users')
+            .where({ email: req.body.email })
+            .then((data) => data[0])
+            .catch((err) => Promise.reject('unable to get user'));
+        } else {
+          Promise.reject('Wrong credentials');
+        }
+      })
+      .catch((err) => Promise.reject('Wrong credentials'))
+  );
 };
 
-module.exports = { handleSignin: handleSignin };
+const getAuthTokenId = () => {
+  console.log('Auth Ok');
+};
+
+const signToken = (email) => {
+  const jwtPayload = { email };
+  return jwt.sign(jwtPayload, 'JWT_SECRET', { expiresIn: '2 days' });
+};
+
+const createSession = (user) => {
+  const { email, id } = user;
+  const token = signToken(email);
+  return { success: 'true', userId: id, token };
+};
+
+const signinAuthentication = (db, bcrypt) => (req, res) => {
+  const { authorization } = req.headers;
+  return authorization
+    ? getAuthTokenId
+    : handleSignin(db, bcrypt, req, res)
+        .then((data) => {
+          return data.id && data.email
+            ? createSession(data)
+            : Promise.reject('Wrong credentials');
+        })
+        .then((session) => res.json(session))
+        .catch((err) => res.status(400).json(err));
+};
+
+module.exports = { signinAuthentication: signinAuthentication };
--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get('/', (req, res) => {
   res.send('Really?');
 });
 
-app.post('/signin', signin.handleSignin(db, bcrypt));
+app.post('/signin', signin.signinAuthentication(db, bcrypt));
 
 app.post('/register', (req, res) => {
   register.handleRegister(req, res, db, bcrypt);
